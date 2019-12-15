d739c8cd61e7d5caa2de43260b28c9789e8f7116
charlesng <charlesng0209@gmail.com>
2019-12-15 22:25:04 +0100

    Fix eslint issue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
+import Baseline from './components/baseline.vue';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
